56eaeea5abfc39e63df1c17e90d6bc71c187256d
gaogao <527518190@qq.com>
2021-12-28 16:54:18 +0800

    添加ci

--- .umirc.ts
+++ .umirc.ts
@@ -3,5 +3,5 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   title: '升高组件',
   outputPath: 'docs-dist',
-  // more config: https://d.umijs.org/config
+  publicPath: '/rc/',
 });
